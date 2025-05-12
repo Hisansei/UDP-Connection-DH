185b7e60686984892a002d3990ba0336621f9ad1
Hisansei <1144204560@u.icesi.edu.co>
2025-05-12 18:51:21 -0500

    Project creation

---
+++ src/ui/PeerH.java
@@ -0,0 +1,26 @@
+package ui;
+
+import util.UDPconnection;
+import java.util.Scanner;
+
+public class PeerH {
+    public static void main(String[] args) {
+        UDPconnection udpConnection = UDPconnection.getInstance();
+        udpConnection.setPort(5000);
+        udpConnection.start();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String destIp = "127.0.0.1";
+            int destPort = 0;
+            
+            destPort = 5001;
+            destIp = "127.0.0.1";
+
+            System.out.print("Write a message: ");
+            String message = "[A]: " + scanner.nextLine();
+            udpConnection.sendMessage(message, destIp, destPort);
+        }
+    }
+}
